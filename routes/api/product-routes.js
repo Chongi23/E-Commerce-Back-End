2e890a8187f2cd81b0a084b3ecfe618f25591b14
Krysta Gonzalez <gnosisknows28@gmail.com>
2023-09-24 09:43:50 -0500

    delete product route working

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -119,8 +119,8 @@ router.put('/:id', (req, res) => {
 router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
   try {
-
-    await ProductTag.destroy({
+     const productData =
+    await Product.destroy({
       where: {
         id: req.params.id,
       },
